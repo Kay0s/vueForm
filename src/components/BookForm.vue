5fed4a72acc2dbefecad74a7b78349814a93afd7
Kay0s <hamilton.kristina@gmail.com>
2021-04-20 18:37:12 -0500

    refactored the call to bookSumbit() and passed tests

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -28,7 +28,7 @@ export default {
   },
   methods: {
     bookSubmit(bookData) {
-      this.$emit("addBook", bookData.bookTitle, bookData.bookAuthor);
+      this.$emit("addBook", bookData);
     }
   }
 };

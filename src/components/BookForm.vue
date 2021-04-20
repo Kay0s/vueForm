f470d58ab744667fd2eaa3be7334fa1392d83df9
Kay0s <hamilton.kristina@gmail.com>
2021-04-20 14:35:21 -0500

    added labels to radio and checkbox elements

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -2,6 +2,14 @@
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
     <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div id="checkbox">
+    <label for="finishedReading">Finished Reading</label>
+    <input type="checkbox" value="bookData.finishedReading" v-model="bookData.finishedReading" />
+    </div>
+    <label for="borrowed">borrowed</label>
+    <input v-model="bookData.ownership" type="radio" name="borrowed" value="borrowed">
+    <label for="bought">bought</label>
+    <input v-model="bookData.ownership" type="radio" name="bought" value="bought">
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>

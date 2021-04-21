54950f7638eaa34443aeac399ce7ecf8a6d36cb7
Kay0s <hamilton.kristina@gmail.com>
2021-04-21 11:45:51 -0500

    added ul element with v-for element filteredBooks and passed test

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -8,6 +8,9 @@
     <select v-model="holding">
       <option v-for='filter in filters' :key='filter.id' :filter='filter'>{{ filter }}</option>
     </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
   </div>

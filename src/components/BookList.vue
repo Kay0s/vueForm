4dbe509c15ed526c668daf8cef73c9a9a9012ae4
Kay0s <hamilton.kristina@gmail.com>
2021-04-22 10:09:22 -0500

    replaced v-for=book in books with v-for=books in serachedBooks

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,7 +3,7 @@
     <h1>{{title}}</h1>
     <input type="text" placeholder="Search Books" v-model="searchInput">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr><h2>Filtered Books by Ownership</h2>
     <select v-model="holding">
